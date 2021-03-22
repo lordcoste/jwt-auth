90c4e0638cc2f3d20a1a63be41e88683cec4baae
Stefano Colao <lordcoste@MBP-di-Stefano.station>
2021-03-22 04:09:33 +0100

    laravel 5.8 support: fire is deprecated, ue dispatch

--- src/Commands/JWTGenerateCommand.php
+++ src/Commands/JWTGenerateCommand.php
@@ -36,7 +36,7 @@ class JWTGenerateCommand extends Command
      *
      * @return void
      */
-    public function fire()
+    public function dispatch()
     {
         $key = $this->getRandomKey();
 
